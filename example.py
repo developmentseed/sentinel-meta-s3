330025ffd04e1ab3c1d013daa0c3894fc3dcef3a
Scisco <scisco7@gmail.com>
2016-03-22 11:36:59 -0400

    update example

--- example.py
+++ example.py
@@ -1,23 +1,14 @@
 import logging
-from datetime import date, timedelta
-from scrawler.main import generate_metadata
+from datetime import date
+from scrawler.main import range_metadata
 
 
 def main():
 
-    start_date = date(2015, 11, 28)
-    end_date = date(2015, 12, 31)
+    start_date = date(2015, 9, 2)
+    end_date = date(2015, 9, 10)
 
-    delta = end_date - start_date
-
-    dates = []
-
-    for i in range(delta.days + 1):
-        dates.append(start_date + timedelta(days=i))
-
-    for d in dates:
-        print('Getting metadata of {0}-{1}-{2}'.format(d.year, d.month, d.day))
-        generate_metadata(d.year, d.month, d.day, '.')
+    return range_metadata(start_date, end_date, '.', 2)
 
 
 if __name__ == '__main__':
