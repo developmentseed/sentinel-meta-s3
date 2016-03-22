b63839837fc530d8b42a0c74331677f2ae4a8572
Scisco <scisco7@gmail.com>
2016-03-22 17:50:36 -0400

    add multiple writers

--- example.py
+++ example.py
@@ -1,14 +1,14 @@
 import logging
 from datetime import date
-from sentinel_s3 import range_metadata
+from sentinel_s3 import range_metadata, s3_writer
 
 
 def main():
 
-    start_date = date(2016, 2, 1)
-    end_date = date(2016, 3, 22)
+    start_date = date(2015, 9, 2)
+    end_date = date(2015, 9, 2)
 
-    return range_metadata(start_date, end_date, '.', 20)
+    return range_metadata(start_date, end_date, '.', 0, s3_writer)
 
 
 if __name__ == '__main__':
